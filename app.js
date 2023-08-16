032d6a3a40f8e1daa8400935af6fb4d5a2853f24
VinayCheripally <vinaycheripally1@gmail.com>
2023-08-16 15:01:51 +0530

    add

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+var path = require("path");
 
 require("dotenv").config();
 
-const app = express();
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
-
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }).then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
   });
 
+  const mySchema = new mongoose.Schema({
+    username: String,
+    password: String
+  });
+
+  const MyModel = mongoose.model('MyModel', mySchema);
+  app.use('/',bodyParser.urlencoded({ extended: true }));
+
+  app.use('/',express.static('foodreview'));
+
   app.get('/', (req, res) => {
-    res.send('A simple Node App is '
-        + 'running on this server')
-    res.end()
-})
+    res.sendFile(__dirname+"/foodreview/html/login.html");
+  })
+  
+  app.post('/', (req, res) => {
+    const myData = new MyModel(req.body);
+    myData.save()
+      .then(() => {
+        res.send('Data saved successfully!');
+      })
+      .catch(() => {
+        res.send('Error saving data.');
+      });
+  });
 
 
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+console.log(`Server is running on port ${PORT}.`);
 });
+
+
