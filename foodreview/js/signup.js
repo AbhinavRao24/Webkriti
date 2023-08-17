4585efee36cada44c42618454186bb78e9675284
unknown <pabhinav2412@gmail.com>
2023-08-17 20:16:54 +0530

    your message

---
+++ foodreview/js/signup.js
@@ -0,0 +1,52 @@
+// script.js
+
+const slides = document.querySelectorAll(".slide");
+let slideIndex = 0;
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+showSlides();
+
+// signup.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const signupForm = document.getElementById("signup-form");
+    
+    signupForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      validateForm();
+    });
+    
+    function validateForm() {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirm-password").value;
+      
+      if (!username || !password || !confirmPassword) {
+        alert("Please fill in all fields.");
+        return;
+      }
+      
+      if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return;
+      }
+      
+      // You can add more validation checks here
+      
+      // If all checks pass, you can proceed with submitting the form
+      alert("Signup successful!");
+      signupForm.reset();
+    }
+  });
+  
