a0b7ab7671f3ab773acff567171412706161d22e
VinayCheripally <vinaycheripally1@gmail.com>
2023-08-22 18:52:13 +0530

    add

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,8 +7,13 @@ const path = require('path');
 // router.post('/register', authController.register);
 // router.post('/login', authController.login);
 router.get('/register',(req,res)=>{
-    const loginPath = path.join(__dirname, '../foodreview/html/login.html');
-    res.sendFile(loginPath);
+    const Path = path.join(__dirname, '../foodreview/html/signup.html');
+    res.sendFile(Path);
+})
+
+router.get('/signin',(req,res)=>{
+    const Path = path.join(__dirname, '../foodreview/html/login.html');
+    res.sendFile(Path);
 })
 
 module.exports = router;
